6531820ce59d2c59d2128bb93a7c09b5f97126c3
Gloomy <544642359@qq.com>
2021-08-23 10:24:43 +0800

    git commit -m "2021.8.23"

---
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
@@ -0,0 +1,19 @@
+package com.sunnyweather.android
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+class SunnyWeatherApplication : Application() {
+    companion object{
+        const val TOKEN = "o7X52NPFFMMKc44a"
+
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+}
---
+++ app/src/main/java/logic/Repository.kt
@@ -0,0 +1,4 @@
+package logic
+
+class Repository {
+}
